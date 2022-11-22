ebaad99a7ec40260d10b55b4a8e4f600f2067b3b
Sloker75 <79275738+Sloker75@users.noreply.github.com>
2022-11-21 21:01:00 -0600

    Work on movement
    Added the material of the plane and also the description of his flight of the plane

---
+++ Assets/Scripts/Plane/PlaneController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _speedToMove;
+    [SerializeField] private float _torqueForce;
+
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void Update()
+    {
+        _rigidbody.velocity = transform.forward * _speedToMove * Time.deltaTime;
+
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddTorque(0f, 0f,-1 * _horizontalInput * _torqueForce);
+        _rigidbody.AddTorque(_verticalInput * _torqueForce, 0f, 0f);
+    }
+}
